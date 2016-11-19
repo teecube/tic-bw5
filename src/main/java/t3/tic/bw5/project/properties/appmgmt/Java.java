02f8700df3be2bf7406e295de3557f64dc2a4fc8
Mathieu Debove <mathieu.debove@fastconnect.fr>
2016-11-19 21:23:06 +0100

    Let's get it start3d!

---
+++ src/main/java/t3/tic/bw5/project/properties/appmgmt/Java.java
@@ -0,0 +1,46 @@
+/**
+ * (C) Copyright 2014-2016 teecube
+ * (http://teecu.be) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package t3.tic.bw5.project.properties.appmgmt;
+
+import java.math.BigInteger;
+
+import t3.xml.XMLVisitor;
+
+public class Java extends XMLVisitor<com.tibco.xmlns.applicationmanagement.Setting.Java> {
+
+	public Java(com.tibco.xmlns.applicationmanagement.Setting.Java visited) {
+		super(visited);
+	}
+
+	@Override
+	public Object addParameter(String key, String value) {
+		if ("prepandClassPath".equals(key)) {
+			visited().setPrepandClassPath(value);
+		} else if ("appendClassPath".equals(key)) {
+			visited().setAppendClassPath(value);
+		} else if ("initHeapSize".equals(key)) {
+			visited().setInitHeapSize(BigInteger.valueOf(Long.parseLong(value)));
+		} else if ("maxHeapSize".equals(key)) {
+			visited().setMaxHeapSize(BigInteger.valueOf(Long.parseLong(value)));
+		} else if ("threadStackSize".equals(key)) {
+			visited().setThreadStackSize(BigInteger.valueOf(Long.parseLong(value)));
+		}
+
+		return visited();
+	}
+
+}
